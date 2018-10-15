36038e5b3b5099f476aa34b0ec537f1556c348f4
Jan Mooij <jan.mooij@incentro.com>
2018-10-15 18:38:52 +0200

    feat(gdpr) add gdpr statement, improves styling

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -6,6 +6,7 @@ export default () => (
       <span>&copy; {(new Date()).getFullYear()}</span>
       <ActiveLink type='footer' href='/algemene-voorwaarden'>Algemene Voorwaarden</ActiveLink>
       <ActiveLink type='footer' href='/verzekeringen/dienstverlening'>Dienstverlening</ActiveLink>
+      <a href="/static/privacy-statement.pdf" target="_blank" rel="noopener">Privacy</a>
     </div>
   </footer>
 )
